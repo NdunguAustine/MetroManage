69f875b80431f5aff6eb919b6fcb20c24c04a9ea
NdunguKariuki1 <3560.2021@students.ku.ac.ke>
2025-02-02 20:29:48 +0300

    models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class DriverConductor(models.Model):
+    userID = models.CharField(max_length=50,unique=True, blank=False, null=False)
+    driverID = models.CharField(max_length=50, unique=True, blank=False, null=False)
+    email =  models.CharField(max_length=50, blank=False, null=False)
+    first_name = models.CharField(max_length=30, blank=False, null=False)
+    last_name = models.CharField(max_length=30, blank=False, null=False)
+    date_of_birth = models.DateField(null=False, blank=False)
+    gender = models.CharField(max_length=10,null=False, blank=False)
+    phone= models.CharField(max_length=20,null=False, blank=False)
+    profile = models.ForeignKey(User, null=False, blank=False,on_delete=models.CASCADE, related_name="driver_profile")
+
+    def __str__(self):
+        return self.driverID
+    
+class  BusDetails(models.Model):
+    busID= models.CharField(max_length=50, unique=True, blank=False, null=False)
+    fleetNumber= models.CharField(max_length=20, blank=False, null=False)
+    numberPlate= models.CharField(max_length=20, blank=False, null=False)
+    driver= models.ForeignKey(DriverConductor, on_delete=models.CASCADE, related_name="bus_driver", blank=True, null=True)
+
+    def __str__(self):
+        return self.numberPlate
+    
+class RouteDetails(models.Model):
+    routeID= models.CharField(max_length=50, unique=True, blank=False, null= False)
+    routeName= models.CharField(max_length=30, blank=False, null=False)
+    startRoute= models.CharField(max_length=50, blank=False, null=False)
+    destination= models.CharField(max_length=50, blank=False, null=False)
+
+    def __str__(self):
+        return self.routeName
+    
+class PaymentDetails(models.Model):
+    paymentID= models.CharField(max_length=50, unique=True, blank=False, null=False)
+    bus= models.ForeignKey(BusDetails, on_delete=models.CASCADE, related_name="bus_payment", blank=False, null=False)
+    amount= models.CharField(max_length=50, blank=False, null=False)
+    referenceCode= models.CharField(max_length=50, blank=False, null=False)
+
+    def __str__(self):
+        return self.bus.numberPlate
+    
+class RouteBus(models.Model):
+    route= models.ForeignKey(RouteDetails, on_delete=models.CASCADE, related_name="bus_route", blank=False, null=False)
+    buses= models.JSONField()
+
+    def __str__(self):
+        return self.route.routeName
--- metromanage/settings.py
+++ metromanage/settings.py
@@ -12,8 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-
+import pymysql
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+pymysql.install_as_MySQLdb
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -75,10 +76,23 @@ WSGI_APPLICATION = 'metromanage.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'metromanage',  # The database name (from the URL)
+        'USER': 'root',   # The username (from the URL)
+        'PASSWORD': 'aq9agm11',  # The password (from the URL)
+        'HOST': 'localhost',  # The host (from the URL)
+        'PORT': '3306',  # The port (from the URL)
+        # 'OPTIONS': {
+        #     'ssl': {'ssl-mode': 'REQUIRED'},  # Required for SSL connection
+        # },
     }
 }
 
