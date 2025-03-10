8cb8e4c812ffaaca42f4c622dfc570ac246db660
NdunguKariuki1 <3560.2021@students.ku.ac.ke>
2025-03-10 21:21:31 +0300

    css

--- metromanage/settings.py
+++ metromanage/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'metromanage',  # The database name (from the URL)
         'USER': 'root',   # The username (from the URL)
-        'PASSWORD': '2000',  # The password (from the URL) aq9agm11
+        'PASSWORD': 'aq9agm11',  # The password (from the URL) aq9agm11
         'HOST': 'localhost',  # The host (from the URL)
         'PORT': '3306',  # The port (from the URL)
         # 'OPTIONS': {
